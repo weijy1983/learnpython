5b3eeb7422853457dd8e680d3d7c1541fa88f3a3
weijy <michong_wjy@163.com>
2017-01-17 14:39:39 +0800

    add fun.py

---
+++ fun.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+# _*_ coding: utf-8 _*_
+
+n1 = 255
+n2 = 1000
+
+print("n1 is: %d"%n1)
+print("n2 is: %d"%n2)
+print("n1 type hex is :0x%x"%n1)
+print("n2 type hex is :0x%x"%n2)
+
