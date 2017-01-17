0ba0391fa889b67706c75c11c496fee3f4a21189
weijy <michong_wjy@163.com>
2017-01-17 15:47:16 +0800

    add abstest.py

---
+++ abstest.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+# _*_ coding: utf-8 _*_
+
+def my_abs(x):
+    if not isinstance(x,(int,float)):
+	    raise TypeError('bad operand type')
+    if x >= 0:
+	    return x
+    else:
+	    return -x
