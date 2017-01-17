d1a3993adc1abd7437003494f01ce508ef0d6146
weijy <michong_wjy@163.com>
2017-01-17 14:25:53 +0800

    add dict.py

---
+++ dict.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+# _*_ coding: utf-8 _*_
+
+d={'weijy':30,'banfufeng':56,'chenyuanzhou':34}
+t=(1,2,3)
+t1=(1,[2,3])
+d1={t[0]:30,t[1]:12,t[2]:69}
+#s=set(t1)
+
+print(d['weijy'])
+print(d1[t[1]])
