e1e1a7218eab1907241b9b101ea1cd978939c8b7
weijy <michong_wjy@163.com>
2017-01-18 16:09:13 +0800

    add decorator.py

---
+++ decorator.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def now():
+    print('2017-01-18')
+
+now()    
