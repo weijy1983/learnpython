19236da554954d3c0bf93488c38a9439e49ea11e
weijy <michong_wjy@163.com>
2017-01-18 16:13:41 +0800

    decolator.py add @log('execute')

--- decorator.py
+++ decorator.py
@@ -1,13 +1,15 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
-def log(func):
-    def wrapper(*args, **kw):
-        print('call %s():' % func.__name__)
-        return func(*args, **kw)
-    return wrapper
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator        
 
-@log
+@log('execute')
 def now():
     print('2017-01-18')
 
