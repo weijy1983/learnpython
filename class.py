8af184917519cf9e06f7a019236fefb8e0b2f672
weijy <michong_wjy@163.com>
2017-01-19 16:10:06 +0800

    __slots__

--- class.py
+++ class.py
@@ -5,6 +5,8 @@
 
 __author__='Weijy'
 
+from types import MethodType
+
 class animal(object):
     def run(self):
         print('This animal is running')
@@ -33,3 +35,26 @@ run_twice(dog())
 run_twice(cat())
 run_twice(tortoise())
 
+class Student(object):
+    __slots__=('name','age','_score','set_age')
+    def get_score(self):
+        print('Score is %.1f' % self._score)
+    
+    def set_score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value            
+
+def set_age(self, age):
+    self.age = age
+
+s = Student()
+s.name = 'Weijy'
+print(s.name)
+s.set_age = MethodType(set_age, s)
+s.set_age(25)
+print(s.age)
+s.set_score(95)
+s.get_score()
