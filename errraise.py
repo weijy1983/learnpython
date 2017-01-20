81485a72fc138925335fb3d7b34abcb089c3b57f
weijy <michong_wjy@163.com>
2017-01-20 14:37:18 +0800

    errraise.py version2

--- errraise.py
+++ errraise.py
@@ -5,13 +5,17 @@
 
 __author__='Weijy'
 
-class FooError(ValueError):
-    pass
-
 def foo(s):
     n = int(s)
     if n == 0:
-        raise FooError('weijy tell you invalid value: %s' %s)
+        raise ValueError('invalid value: %s' % s)
     return 10/n
 
-foo('0')    
+def bar():
+    try:
+        foo('0')
+    except ValueError as e:
+        print('ValueError!')
+        raise
+
+bar()
